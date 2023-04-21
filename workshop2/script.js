1417ef9e25a5236367e9859745285bd99ec5b6f9
Bassil Younes <bassilyounes@gmail.com>
2023-04-21 14:40:42 -0400

    done

---
+++ workshop2/script.js
@@ -0,0 +1,11 @@
+document.getElementById("visitor").addEventListener("click", () => {
+    window.location.href = "visitor.html";
+});
+
+document.getElementById("delivery").addEventListener("click", () => {
+    window.location.href = "delivery.html";
+});
+
+document.getElementById("reception").addEventListener("click", () => {
+    window.location.href = "reception.html";
+});
